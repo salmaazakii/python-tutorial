839538be66f97437f9c6d6d6446c231284d31fb0
salmaazakii <salmazaki700@gmail.com>
2023-09-22 18:04:33 +0300

    solving List

--- list.py
+++ list.py
@@ -4,3 +4,30 @@ for number in numbers:
     if number > max_num:
         max_num = number
 print(max_num)
+
+
+N = int(input())
+i = 0
+arr = []
+temp = []
+result = []
+while i < N:
+    cmd = input()
+    if cmd.find("append") > -1:
+        arr.append(int(cmd.split(" ")[1]))
+    elif cmd.find("insert") > -1:
+        arr.insert(int(cmd.split(" ")[1]),int(cmd.split(" ")[2]))
+    elif cmd.lower() == "print":
+        temp = arr.copy()
+        result.append(temp)
+    elif cmd.find("remove") > -1:
+        arr.remove(int(cmd.split(" ")[1]))
+    elif cmd.lower() == "sort":
+        arr.sort()
+    elif cmd.lower() == "pop":
+        arr.pop()
+    elif cmd.lower() == "reverse":
+        arr.reverse()
+    i+=1
+for j in result:
+    print(j)
